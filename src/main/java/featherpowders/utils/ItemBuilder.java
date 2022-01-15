332d2805e28168e88da99774b001d0c57a401fbc
nahkd123 <nahkd123@gmail.com>
2022-01-15 23:03:30 +0700

    Resolve missing imports for ItemBuilder

--- src/main/java/featherpowders/utils/ItemBuilder.java
+++ src/main/java/featherpowders/utils/ItemBuilder.java
@@ -3,6 +3,8 @@ package featherpowders.utils;
 import java.util.Arrays;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
